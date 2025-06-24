a1dca1fec00c425dd401b578907c7d4ee1aae45e
faiyaz <55284898+faiyazz-shaikh@users.noreply.github.com>
2025-06-24 11:05:23 +0530

    Added tests for other delimiters

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -4,9 +4,21 @@ class StringCalculator {
       return 0;
     }
 
-    final delimiter = ',';
+    var delimiter = ',';
+    var numbersToProcess = numbers;
 
-    final numbersToProcess = numbers.replaceAll('\n', delimiter);
+    // Check for custom delimiter -> //[delimiter]\n[numbers…]
+    if (numbers.startsWith('//')) {
+      // Extract the custom delimiter
+      int delimiterEndIndex = numbers.indexOf('\n');
+      delimiter = numbers.substring(2, delimiterEndIndex);
+
+      // Remove the custom delimiter line from the numbers string
+      numbersToProcess = numbers.substring(delimiterEndIndex + 1);
+    }
+
+    // Replace new lines with the delimiter
+    numbersToProcess = numbersToProcess.replaceAll('\n', delimiter);
 
     return numbersToProcess
         .split(delimiter)
